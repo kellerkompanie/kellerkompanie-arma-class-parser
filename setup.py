709e7a47760fe4cfec8f8ad9d3339096f684892e
Schwaggot <tom.ryan@posteo.de>
2020-03-25 20:42:31 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='armaclassparser',
+   version='0.1.0',
+   description='A parser for Arma 3 class/config files.',
+   author='Schwaggot',
+   author_email='tom.ryan@posteo.de',
+   packages=['armaclassparser'],
+   install_requires=[],
+   test_suite="tests",
+)
